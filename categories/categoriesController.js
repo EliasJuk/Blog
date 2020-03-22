7fbaefdabe07f43eac19b83f9984dfae8480acdc
eliasjuk <eliasp.juk@hotmail.com>
2020-03-22 10:05:47 -0300

    Import slugify para tratar titles para uma url

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
-const category
+const Category = require("./Category")
+const slugfy = require('slugify')
 
 router.get("/admin/categories/new", (req, res) => {
     res.render('admin/categories/new')
@@ -10,7 +11,10 @@ router.get('/categories/save', (req,res) => {
     var title = rq.body.title;
     if(title != undefined){
         //SALVA DB
-        category.create()
+        category.create({
+            title: title,
+            slug: slug
+        })
     }else{
         res.redirect("/admin/categories/new")
     }
