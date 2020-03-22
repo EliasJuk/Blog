494c631cb0cfdd056b431538b177ff1d5b6c84e7
eliasjuk <eliasp.juk@hotmail.com>
2020-03-22 10:40:34 -0300

    Construir uma tabela para mostrar os dados do banco

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const router = express.Router();
 const Category = require("./Category")
-const slugfy = require('slugify')
+const slugify = require('slugify')
 
 router.get("/admin/categories/new", (req, res) => {
     res.render('admin/categories/new')
 })
 
-router.get('/categories/save', (req,res) => {
-    var title = rq.body.title;
+router.post('/categories/save', (req,res) => {
+    var title = req.body.title;
     if(title != undefined){
         //SALVA DB
-        category.create({
+        Category.create({
             title: title,
-            slug: slug
+            slug: slugify(title)
+        }).then( () => {
+            res.redirect("/")
         })
     }else{
         res.redirect("/admin/categories/new")
     }
+})
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render('admin/categories/index', {categories: categories})   
+        //console.log(categories)     
+    })
 })
 
 module.exports = router
