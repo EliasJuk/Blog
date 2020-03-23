4256f180f3d0b7e30d0249edf2fe9dc830bfba0f
eliasjuk <eliasp.juk@hotmail.com>
2020-03-23 16:52:49 -0300

    Carregar conteudo do artigo dentro da textarea com JQuery para edição

--- articles/articleController.js
+++ articles/articleController.js
@@ -59,8 +59,8 @@ router.get("/admin/articles/edit/:id", (req, res) => {
         if(article != undefined){
 
             Category.findAll().then(categories => {
-                res.render("admin/articles/edit", { categories: categories })
-            })  
+                res.render("admin/articles/edit", { categories: categories, article: article })
+            })
                 
         }else{
             res.redirect("/")
