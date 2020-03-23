cbabe60ed70acac6dabbc28aede9b668eff5e379
eliasjuk <eliasp.juk@hotmail.com>
2020-03-23 09:30:19 -0300

    Mostrar Artigos na tela principal

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ const articleController = require("./articles/articleController")
 //ROUTES
     //MAIN
         app.get("/", (req, res) => {
-            res.render("index")
+            Article.findAll().then(articles => {
+                res.render("index", { articles: articles })
+            })
         })
 
 //CONFIG
