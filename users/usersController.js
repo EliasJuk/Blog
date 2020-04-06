27e9078e7f60950018dea16beabdbb1160e6d167
eliasjuk <eliasp.juk@hotmail.com>
2020-04-06 16:58:55 -0300

    Pagina listagem de usuarios

--- users/usersController.js
+++ users/usersController.js
@@ -3,39 +3,41 @@ const router = express.Router()
 const Users = require('./user')
 const bcrypt= require('bcryptjs')
 
+//LIST
+    //FORMULARIO DE LISTAGEM
+        router.get("/admin/users", (req, res) => {
+            Users.findAll().then(users => {
+                res.render("admin/users/index", { users: users })
+            })
+        })
 
-//LISTAR USUARIOS
-    router.get('/admin/users', (req, res) => {
-        res.send("Listagem de usuarios")
-    })
+//CREATE
+    //FORMULARIO DE CRIAÇÃO
+        router.get("/admin/users/create", (req, res) => {
+            res.render('admin/users/create')
+        })
 
-//FORMULARIO DE CRIAÇÃO
-    router.get("/admin/users/create", (req, res) => {
-        res.render('admin/users/create')
-    })
+    //CADASTRA NO BANCO
+    router.post("/users/create", (req, res) => {
+        var email = req.body.email
+        var password = req.body.password
 
-//CADASTRA NO BANCO
-router.post("/users/create", (req, res) => {
-    var email = req.body.email
-    var password = req.body.password
-
-    var salt = bcrypt.genSaltSync(10)
-    var hash = bcrypt.hashSync(password, salt)
-
-    Users.create({
-        email: email,
-        password: hash
-    }).then(() => {
-        res.redirect('/')
-    }).catch((err) => {
-        res.redirect('/')
-    })
-})
-
-//LISTAGEM DE USUARIOS
-    router.get("/users/list", (req, res) => {
-        Users.findAll().then(users => {
-            res.json(users);
+        Users.findOne({ where: { email: email } }).then( user => {
+            if(user == undefined){
+                var salt = bcrypt.genSaltSync(10)
+                var hash = bcrypt.hashSync(password, salt)
+            
+                Users.create({
+                    email: email,
+                    password: hash
+                }).then(() => {
+                    res.redirect('/')
+                }).catch((err) => {
+                    res.redirect('/')
+                })
+            }else{
+                res.redirect("/admin/users/create")
+            }
         })
     })
 
